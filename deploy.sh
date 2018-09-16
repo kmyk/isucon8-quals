5617618448c7afed7ba2e5a62b13dae9975b3db6
Kimiyuki Onaka <kimiyuki95@gmail.com>
2018-09-16 11:26:40 +0900

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,17 @@ ip2=118.27.20.60
 ip3=118.27.29.133
 
 scp -r etc isucon@${ip1}:
-scp -r usr isucon@${ip1}:
 scp torb/db/schema.sql isucon@${ip1}:torb/db/
 scp torb/webapp/env.sh isucon@${ip1}:torb/webapp/
 scp torb/webapp/python/app.py isucon@${ip1}:torb/webapp/python/
 scp -r torb/webapp/python/templates isucon@${ip1}:torb/webapp/python/
 scp etc/systemd/system/torb.python.service isucon@${ip1}:etc/systemd/system/
+scp etc/h2o/h2o.conf isucon@${ip1}:etc/h2o/
 
-ssh isucon@${ip} <<EOF
+ssh isucon@${ip1} <<EOF
 set -e
 sudo cp etc/systemd/system/torb.python.service /etc/systemd/system/torb.python.service
+sudo cp etc/h2o/h2o.conf /etc/h2o/h2o.conf
 sudo systemctl daemon-reload
 sudo systemctl restart mariadb
 sudo systemctl restart torb.python.service
