0ff491c33dd960e094f626ffa9a389a745f74d69
Keishi Kawada <megumish@exploitouri.st>
2018-09-16 15:42:54 +0900

    bug fix

--- deploy.sh
+++ deploy.sh
@@ -20,12 +20,15 @@ scp etc/h2o/h2o.conf isucon@${ip}:etc/h2o/
 
 ssh isucon@${ip} <<EOF
 set -e
+mkdir -p /home/isucon/run/torb.python
 sudo cp etc/systemd/system/torb.python.service /etc/systemd/system/torb.python.service
 sudo cp etc/systemd/system/torb.python.socket /etc/systemd/system/torb.python.socket
 sudo cp -r etc/tmpfiles.d/torb.python.conf /etc/tmpfiles.d/torb.python.conf
 sudo cp etc/h2o/h2o.conf /etc/h2o/h2o.conf
 sudo systemctl daemon-reload
-sudo systemctl restart torb.python.service
-sudo systemctl restart torb.python.socket
+sudo systemctl stop torb.python.socket
+sudo systemctl stop torb.python.service
+sudo systemctl start torb.python.socket
+sudo systemctl start torb.python.service
 sudo systemctl restart h2o
 EOF
